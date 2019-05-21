1581d5700ad44295ca428d5c58d348c897114d27
Jason Kai <kaitj@users.noreply.github.com>
2019-05-21 13:36:35 -0400

    add datalad-osf install script

---
+++ install_scripts/30.install_datalad-osf_by_source_sudo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# pip3
+curl https://bootstrap.pypa.io/get-pip.py | python3 
+
+# datalad-osf
+if [ -e /tmp/datalad-osf ]
+then
+    rm -rf /tmp/datalad-osf
+fi 
+
+git clone https://github.com/khanlab/datalad-osf /tmp/datalad-osf && cd /tmp/datalad-osf
+pip3 install -r requirements.txt && python3 setup.py install && cd 
